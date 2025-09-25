7a999c09dc0a2311d09a7810ada289b1293ae63a
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-09-25 17:32:48 +0300

    add first script for t5 inference

---
+++ src/t5/utils.py
@@ -0,0 +1,10 @@
+import torch
+
+
+def select_optimal_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
