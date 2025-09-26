ddeeda21bd118b31ee71cb8467af4d73b6d8a32b
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-09-26 12:25:12 +0300

    add custom HF dataset

--- src/t5/main.py
+++ src/t5/main.py
@@ -1,4 +1,4 @@
-# Load model directly
+import sys
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
@@ -6,18 +6,57 @@ from utils import select_optimal_device
 
 import time
 
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+    DataCollatorForSeq2Seq,
+    TrainingArguments,
+    Trainer,
+)
+
+
+def preprocess_fn(examples, tokenizer: T5Tokenizer, max_input_length: int, max_output_length: int, input_prefix: str):
+    inputs = [f'{input_prefix}{q}' for q in examples['question']]
+    targets = examples['answer']
+
+    model_inputs = tokenizer(inputs, max_length=max_input_length,
+                             truncation=True, padding="max_length")
+    labels = tokenizer(targets, max_length=max_output_length,
+                       truncation=True, padding="max_length")
+    model_inputs['labels'] = labels['input_ids']
+    model_inputs['original_inputs'] = inputs
+
+    return model_inputs
+
+
 if __name__ == "__main__":
     device = select_optimal_device()
+
     tokenizer = T5Tokenizer.from_pretrained(
         "google/flan-t5-small", legacy=False)
     model = T5ForConditionalGeneration.from_pretrained(
         "google/flan-t5-small", device_map=device)
 
-    input_text = "translate English to German: How old are you?"
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
+    ds = load_dataset("basavyr/qa_tocqueville")
 
-    start = time.perf_counter_ns()
-    outputs = model.generate(input_ids)
-    print(tokenizer.decode(outputs[0]))
-    duration = (time.perf_counter_ns() - start)*1e-3
-    print(f'{duration:.4f} us')
+    tokenized_dataset = ds.map(
+        lambda examples: preprocess_fn(
+            examples,
+            tokenizer=tokenizer,
+            max_input_length=512,
+            max_output_length=512,
+            input_prefix="Question: "
+        ),
+        batched=True,
+        batch_size=2
+    )
+
+    tokenized_dataset.set_format(type="torch",
+                                 columns=['id', 'question', 'answer', 'input_ids', 'attention_mask', 'labels', 'original_inputs'])
+
+    # create train and test data
+    tokenized_dataset = tokenized_dataset["train"].train_test_split(
+        test_size=0.1)
+    train_dataset = tokenized_dataset["train"]
+    eval_dataset = tokenized_dataset["test"]
